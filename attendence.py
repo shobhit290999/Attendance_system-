ca57d573d6fb0b6738875cb535a8906bdbb53b16
shobhit290999 <75937290+shobhit290999@users.noreply.github.com>
2021-09-07 23:34:38 +0530

    Update attendence.py

--- attendence.py
+++ attendence.py
@@ -1,30 +1,23 @@
+f = open('attandance.txt','w')
+s = "Name"
+f.write(s)
+for i in range(int(input('Enter the number of student: '))):
+ 
+ s = '\t'+input('Enter Name: ')
+ f.write(s)
+ 
+f.close()
 import time
-f = open('attendence.txt','r+')
-data = f.read().split('\n')[0].split('\t')
+f = open('attandance.txt','r+')
+data = f.read()
+data = data.split('\n')[0].split('\t')
 s = f"\n{time.strftime('%d-%m')}"
 f.write(s)
 print('Enter P for Present and A for abscent')
 for i in data[1:]:
-    
-    s = f"\t{input(f'{i}-->').upper()}"
-    
-    f.write(s)
-    
+ 
+ s = f"\t{input(f'{i}-->').upper()}"
+ 
+ f.write(s)
+ 
 f.close()
-
-f = open('attendence.txt','r')
-data  = f.read().split('\n')
-data2=[]
-for i in data:
-    data2.append(i.split('\t'))
-
-
-for i in range(1,8):
-    l=[]
-    for j in range(0,len(data2)):
-        l.append(data2[j][i])
-    present = l.count('P') 
-    absent = l.count('A')
-    print(l)
-    print(f"present:{present}")
-    print(f"absent:{absent}")
